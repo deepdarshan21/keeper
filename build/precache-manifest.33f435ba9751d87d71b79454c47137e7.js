7df7038efff41543aab0e55d6c779756934aab7f
Deepdarshan <deepdarshan21@gmail.com>
2021-10-22 22:55:22 +0530

    final

---
+++ build/precache-manifest.33f435ba9751d87d71b79454c47137e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bff37a9b37e622dad6c9959fb642d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c979f6f2b7448a094d4",
+    "url": "/static/js/2.ec753225.chunk.js"
+  },
+  {
+    "revision": "bec24f383402368dc7b0",
+    "url": "/static/js/main.6209a1ce.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
